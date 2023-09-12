69f7fde54c97eb222c9d31bf85acc004039f1706
Nemanja Stankovic <nemanjastan005@gmail.com>
2023-09-12 09:05:43 +0200

    Project setup

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from 'styled-components';
+
+export const App = () => {
+    return (
+        <Container>
+            <h1>AAAAAAAAAA</h1>
+        </Container>
+    )
+};
+
+const Container = styled.div`
+    background-color: #000;
+`;
