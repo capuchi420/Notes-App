227804fa73c08b09a617463b33f24ba51c9d1175
Nemanja Stankovic <nemanjastan005@gmail.com>
2023-09-14 20:37:24 +0200

    Work in progress

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,50 @@
 import React from "react";
 import styled from 'styled-components';
+import Note from "./components/Note";
 
 export const App = () => {
     return (
         <Container>
-            <h1>AAAAAAAAAA</h1>
+            <form>
+                <input />
+                <button>ADD</button>
+            </form>
+            <div>
+                <Note />
+            </div>
         </Container>
     )
 };
 
 const Container = styled.div`
-    background-color: #000;
+    display: flex;
+    align-items: center;
+    margin-top: 3rem;
+    flex-direction: column;
+
+    form{
+        min-width: 300px;
+        width: 70%;
+        max-width: 800px;
+
+        input{
+            width: 80%;
+            padding: .8rem;
+            border: none;
+
+            &:focus{
+                outline: none;
+            }
+        }
+
+        button{
+            width: 20%;
+            padding: .8rem 1.6rem;
+            border: none;
+        }
+    }
+
+    div{
+        
+    }
 `;
