67804bbf86ef5d32232fb281660c33a6a4caaf4b
Nemanja Stankovic <nemanjastan005@gmail.com>
2023-09-20 18:08:57 +0200

    Project done

--- app/src/components/Note.jsx
+++ app/src/components/Note.jsx
@@ -4,6 +4,24 @@ import styled from 'styled-components';
 export default function Note(props) {
   const handleDone = (e) => {
     e.target.closest("main").style.backgroundColor = "#d5d5d5";
+
+    var newCookie = "";
+    var x = document.cookie.slice(5, document.cookie.length).split('=');
+    for(let i = 0; i < x.length; i++){
+      if(props.txt !== x[i]){
+        if(i === x.length - 1){
+          newCookie += `${x[i]}`;
+        }else{
+          newCookie += `${x[i]}=`;
+        }
+      }
+      if(i === x.length - 1){
+        newCookie = newCookie.slice(0, newCookie.length - 1);
+      }
+    }
+    document.cookie = `note=; expires=20 Jun 1999 00:00:00 UTC;`;
+    document.cookie = `note=${newCookie}; expires=20 Jun 9999 00:00:00 UTC;`;
+    window.location.reload();
   };
 
   const handleDelete = (e) => {
